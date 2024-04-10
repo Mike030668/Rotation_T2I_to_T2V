ae5318b0d3d35ca92f7011134dfbd2a10f838a8e
Михаил Пузицкий <gretawolf@live.ru>
2024-04-10 18:49:20 +0300

    loads

---
+++ data/get_datasets.py
@@ -0,0 +1,11 @@
+# Collected prepared pytorch datasets linls for loading
+
+
+TGIF_DATASETS = {"both_ways": { "300_pxls_1" : "1u3vEgamCwj-Au6xk6beWpkcVMT7UAWgB",
+                                "500_pxls_1" : "1YHHzESWX6SfUn3In19UwySDUW1Hvv9w6",
+                               "discription" : " embeddeed with Kand_22 torch dataset"
+                               
+
+}
+
+}
