efd715f901bb94514c9cd0d0c8a9f7148319a147
Михаил Пузицкий <gretawolf@live.ru>
2024-04-11 22:16:29 +0300

    simple spliter

--- build_models/spliters.py
+++ build_models/spliters.py
@@ -37,7 +37,7 @@ class Increment_spliter(nn.Module):
         # Apply CrossAttentionLayer text_hidden_states and increment
         cross_text_rise = self.cross_attention(text_hidden_states, increment)
 
-        prior_embeds = F.normalize(prior_embeds, p=2.0, dim=2)
+        #prior_embeds = F.normalize(prior_embeds, p=2.0, dim=2)
 
         prior_trained = self.lin_start(prior_embeds)
 
@@ -58,3 +58,55 @@ class Increment_spliter(nn.Module):
         # Use lin_final for the last step
         out = self.lin_final(out).permute(0, 2, 1)
         return out
+
+
+class Simple_spliter(nn.Module):
+    def __init__(self, emb_dim):
+          super(Simple_spliter, self).__init__()
+          ### New layers:
+          self.emb_dim = emb_dim
+          self.lin_increment= nn.Linear(1, emb_dim)
+          self.lin_0  = nn.Linear(1280, 1280)
+          self.dropout = nn.Dropout(0.3)
+          self.lin_1  = nn.Linear(79, 128)
+          self.lin_2 = nn.Linear(128, 64)
+          self.lin_3 = nn.Linear(64, 32)
+          self.lin_4 = nn.Linear(32, 16)
+          self.lin_5 = nn.Linear(16, 8)
+          self.lin_6 = nn.Linear(8, 4)
+          self.lin_7 = nn.Linear(4, 2)
+          self.lin_8 = nn.Linear(2, 1)
+
+    def forward(self, text_hidden_states, prior_embeds, rise):
+          
+          increment = self.lin_increment(rise).unsqueeze(1)
+          pre_out = self.lin_0(prior_embeds)
+          concat_data = torch.concat([text_hidden_states,
+                                      pre_out,
+                                      increment
+                                      ],
+                                      axis = 1)
+          #print(concat_data.shape)
+          concat_data = torch.nn.functional.normalize(concat_data, p=2.0, dim = 1)
+          out = self.lin_1(concat_data.permute(0,2,1))
+          out = nn.ELU()(out)
+          out = self.lin_2(out)
+          out = nn.ELU()(out)
+          out = self.lin_3(out)
+          out = nn.LayerNorm(out.shape[-1], elementwise_affine=False)(out)
+          out = nn.ELU()(out)
+          out = self.dropout(out)
+          out = self.lin_4(out)
+          out = nn.ELU()(out)
+          out = self.lin_5(out)
+          out = nn.LayerNorm(out.shape[-1], elementwise_affine=False)(out)
+          out = nn.ELU()(out)
+          out = self.dropout(out)
+          out = self.lin_6(out)
+          out = nn.ELU()(out)
+          out = self.lin_7(out)
+          out = self.dropout(out)
+          out = nn.ELU()(out)
+          out = self.lin_8(out).permute(0,2,1)
+
+          return out
