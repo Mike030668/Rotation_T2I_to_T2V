155745ea748b8ebba3b04442b6d0c7c6abaaf216
Михаил Пузицкий <gretawolf@live.ru>
2024-04-16 16:35:33 +0300

    old dataset

--- data/get_datasets.py
+++ data/get_datasets.py
@@ -1,11 +1,16 @@
 # Collected prepared pytorch datasets linls for loading
 
 
-TGIF_DATASETS = {"both_ways": { "300_pxls_1" : "1u3vEgamCwj-Au6xk6beWpkcVMT7UAWgB",
-                                "500_pxls_1" : "1YHHzESWX6SfUn3In19UwySDUW1Hvv9w6",
-                               "discription" : " embeddeed with Kand_22 torch dataset"
+TGIF_DATASETS = {"both_ways": { "300_pxls_1" : "1u3vEgamCwj-Au6xk6beWpkcVMT7UAWgB",  # file_name bothway_embed_data_min_300_1.pth  
+                                "500_pxls_1" : "1YHHzESWX6SfUn3In19UwySDUW1Hvv9w6",  # file_name bothway_embed_data_min_500_1.pth  
+                                "discription" : " embeddeed with Kand_22 torch dataset includ text_embedd and last hiden state"
+                               
+
+}, 
+                 "both_ways_old": { "300_pxls" : "1MJ3sQ0EJyZVfXLGSxbe0WMI2Fk4ZyAvA",   # file_name bothway_embed_data_min_300_start.pth                                 
+                                    "discription" : " embeddeed with Kand_22 torch dataset included only last hiden state"
                                
 
 }
 
-}
+}  
