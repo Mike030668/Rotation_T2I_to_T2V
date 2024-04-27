6a4f2d59be553d37beea0840bcc3e651dd047fe9
Михаил Пузицкий <gretawolf@live.ru>
2024-04-27 20:57:22 +0300

    test_incr_2

--- build_models/test_spliters.py
+++ build_models/test_spliters.py
@@ -5,9 +5,9 @@ from  build_models.special_layers import CrossAttentionLayer, RotaryPositionalEm
 from  build_models.special_layers import ImprovedBlock, ImprovedBlock_next
 
 
-class Increment_spliter(nn.Module):
+class Increment_spliter_1(nn.Module):
     def __init__(self, emb_dim, max_seq_len, device='cpu'):
-        super(Increment_spliter, self).__init__()
+        super(Increment_spliter_1, self).__init__()
         # add CrossAttentionLayer
         self.cross_attention = CrossAttentionLayer(emb_dim).to(device)
         # add RotaryPositionalEmbedding
@@ -64,6 +64,66 @@ class Increment_spliter(nn.Module):
         return out
 
 
+class Increment_spliter_2(nn.Module):
+    def __init__(self, emb_dim, max_seq_len, device='cpu'):
+        super(Increment_spliter_2, self).__init__()
+        # add CrossAttentionLayer
+        self.cross_attention = CrossAttentionLayer(emb_dim).to(device)
+        # add RotaryPositionalEmbedding
+        self.pos_encoder = RotaryPositionalEmbedding(emb_dim, max_seq_len, device).to(device)
+
+        # others
+        self.emb_dim = emb_dim
+        self.lin_increment= nn.Linear(1, emb_dim).to(device)
+        self.dropout = nn.Dropout(0.3).to(device)
+        self.lin_start = nn.Linear(emb_dim, emb_dim).to(device)
+
+        self.block_1 = ImprovedBlock(232, 256, 0.3).to(device)
+        self.block_2 = ImprovedBlock(256, 128, 0.3).to(device)
+        self.block_3 = ImprovedBlock(128, 64, 0.3).to(device)
+        self.block_4 = ImprovedBlock(64, 32, 0.3).to(device)
+        self.block_5 = ImprovedBlock(32, 16, 0.3).to(device)
+        self.block_6 = ImprovedBlock(16, 8, 0.3).to(device)
+        self.block_7 = ImprovedBlock(8, 4, 0.3).to(device)
+
+        self.lin_final = nn.Linear(4, 1).to(device)
+
+
+    def forward(self, text_hidden_states, prior_embeds, rise):
+
+        increment = self.lin_increment(rise).unsqueeze(1)
+        increment =  nn.LeakyReLU()(increment)
+        
+        # Use RotaryPositionalEmbedding
+        text_hidden_states = self.pos_encoder(text_hidden_states)
+        # Apply CrossAttentionLayer text_hidden_states and increment
+        cross_text_rise = self.cross_attention(text_hidden_states, increment)
+
+        # nomolise espessialy for regress
+        prior_embeds =  torch.nn.functional.normalize(prior_embeds, p=2.0, dim = -1)
+        prior_trained = self.lin_start(prior_embeds)
+        cross_text_prior = self.cross_attention(text_hidden_states, prior_trained)
+
+
+        concat_data = torch.concat([text_hidden_states,
+                                    prior_trained,
+                                    cross_text_prior,
+                                    cross_text_rise],
+                                    axis=1)
+        
+        concat_data = torch.nn.functional.normalize(concat_data, p=2.0, dim = 1)
+        #print("concat_data", concat_data.shape)
+        out = self.block_1(concat_data.permute(0, 2, 1))
+        out = self.block_2(out)
+        out = self.block_3(out)
+        out = self.block_4(out)
+        out = self.block_5(out)
+        out = self.block_6(out)
+        out = self.block_7(out)
+
+        # Use lin_final for the last step
+        out = self.lin_final(out).permute(0, 2, 1)
+        return out
 
 
 
@@ -120,8 +180,8 @@ class DualBranchSpliter(nn.Module):
         cross_text_prior = self.cross_attention(text_hidden_states, prior_trained)
 
         concat_cross = torch.concat([cross_text_rise,
-                    cross_text_prior],
-                    axis=1)
+                                    cross_text_prior],
+                                    axis=1)
 
 
         base_output = self.down_block_1(concat_base.permute(0, 2, 1))
