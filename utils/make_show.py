1ad39aa129c99ff9efe2a04b2e8753beba879f66
Михаил Пузицкий <gretawolf@live.ru>
2024-05-13 17:31:49 +0300

    mp4

--- utils/make_show.py
+++ utils/make_show.py
@@ -3,6 +3,13 @@ import PIL
 import base64
 from IPython import display
 
+
+import cv2
+import numpy as np
+# Функции для возпроизведение видео с результатом
+from moviepy.editor import *
+
+
 def make_gif(set_dir_pil,
              out_name = "my_awesome.gif",
              duration = 10,
@@ -23,3 +30,26 @@ def show_gif(fname):
     with open(fname, 'rb') as fd:
         b64 = base64.b64encode(fd.read()).decode('ascii')
     return display.HTML(f'<img src="data:image/gif;base64,{b64}" />')
+
+
+
+
+def make_mp4(pil_imgs, out_name, width, height, duration=20):
+  # Чем больше кадров в секунду, тем быстее видео будет проигрываться
+    out = cv2.VideoWriter(out_name, cv2.VideoWriter_fourcc(*'MP4V'), duration, (width, height))
+
+    # В цикле добавляем каждый кадр в видео (делаем предобработку кадра - меняем каналы с RGB в BGR
+    # это нужно потому что cv2 воспринимает каналы как BGR)
+    for frame in pil_imgs:
+      out.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+
+    # Закрываем объект для создания видео
+    out.release()
+
+
+def show_mp4(path, width, duration=20):
+    # Извлекаем видео из заданного пути (куда мы ранее записыли видео через cv2)
+    clip=VideoFileClip(path)
+
+    # Отображаем видео в колабе
+    return clip.ipython_display(width=width, maxduration = duration)
