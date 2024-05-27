5b6202dfdd56b9e7667929d9291e50423fc7a53b
Михаил Пузицкий <gretawolf@live.ru>
2024-05-27 18:44:01 +0300

    DualBranchSpliterSA

--- build_models/test_spliters.py
+++ build_models/test_spliters.py
@@ -1019,3 +1019,83 @@ class IncrementSpliterSAT(nn.Module):
 
         out = self.lin_final(out).permute(0, 2, 1)
         return out
+
+
+
+class DualBranchSpliterSA(nn.Module):
+    def __init__(self, emb_dim = EMB_DIM, max_seq_len = MAX_SEQ_LEN_K22, device='cpu'):
+        super(DualBranchSpliterSA, self).__init__()
+
+        self.emb_dim = emb_dim
+        self.device = device
+
+        self.csa = ConsistentSelfAttentionBase(emb_dim).to(device)
+        self.cross_attention = CrossAttentionLayer(emb_dim).to(device)
+        self.pos_encoder = RotaryPositionalEmbedding(emb_dim, max_seq_len, device).to(device)
+        self.lin_increment = nn.Linear(1, emb_dim).to(device)
+        self.lin_start = nn.Linear(emb_dim, emb_dim).to(device)
+
+        
+        # Rise branch for handling rise-influenced data
+        self.down_block_1 = nn.Sequential(
+            ImprovedBlock(158, 256, 0.3),
+            ImprovedBlock(256, 128, 0.3),
+            ImprovedBlock(128, 64, 0.3),
+            ImprovedBlock(64, 32, 0.3),
+        ).to(device)
+
+        # Rise branch for handling rise-influenced data
+        self.down_block_2 = nn.Sequential(
+            ImprovedBlock(154, 256, 0.3),
+            ImprovedBlock(256, 128, 0.3),
+            ImprovedBlock(128, 64, 0.3),
+            ImprovedBlock(64, 32, 0.3),
+        ).to(device)
+
+        # Rise branch for handling rise-influenced data
+        self.down_block_fin = nn.Sequential(
+            ImprovedBlock(64, 32, 0.3),
+            ImprovedBlock(32, 16, 0.3),
+            ImprovedBlock(16, 1, 0.3),
+        ).to(device)
+
+    def forward(self, text_hidden_states, prior_embeds, rise):
+        # Positional encoding applied to text hidden states
+        text_hidden_states = self.pos_encoder(text_hidden_states)
+
+        # Base branch processing
+        prior_trained = self.lin_start(prior_embeds)
+
+        # Rise branch processing
+        increment = self.lin_increment(rise).unsqueeze(1)
+        increment = nn.LeakyReLU()(increment)
+
+        concat_base = torch.concat([text_hidden_states,
+                                    prior_trained,
+                                    increment],
+                                    axis=1)
+        
+        self_attn = self.csa(concat_base)
+        concat_base = torch.concat([concat_base,
+                                    self_attn],
+                                    axis=1)
+
+        cross_text_rise = self.cross_attention(text_hidden_states, increment)
+        cross_text_prior = self.cross_attention(text_hidden_states, prior_trained)
+
+        concat_cross = torch.concat([cross_text_rise,
+                                    cross_text_prior],
+                                    axis=1)
+
+
+        base_output = self.down_block_1(concat_base.permute(0, 2, 1))
+
+        cross_output = self.down_block_2(concat_cross.permute(0, 2, 1))
+
+
+        concat_out = torch.concat([base_output,
+                                  cross_output],
+                                  axis=-1)
+
+        out = self.down_block_fin(concat_out).permute(0, 2, 1)
+        return out
