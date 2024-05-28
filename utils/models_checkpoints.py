a1e7ab519c486e9d2550bdfd4cfeed311f859f9f
Михаил Пузицкий <gretawolf@live.ru>
2024-05-28 16:31:18 +0300

    models_checkpoints

---
+++ utils/models_checkpoints.py
@@ -0,0 +1,15 @@
+splitters_checkpoints = {
+                        "Increment_spliter_next":{"key_cpt": "1YbTi7q5MWAA5DEJCHgviKisD5MgjLy59",
+                                                  "discription": """
+                                                                Trained with CombinedLoss_cos_trans ab_1
+                                                                pover rote 1 data_300_500pxls all_ways best mse
+                                                                """
+                                            },
+
+                        "DualBranchSpliter":{"key_cpt": "1gmH8NfXkB2LZ4MJ8ATjXp72mpkkqqt4E",
+                                            "discription": """
+                                                            Trained with base loss
+                                                            pover rote 1 data_300pxls all_ways best loss
+                                                            """
+                                            },
+}
